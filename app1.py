13a63348055bffa04b07dbbe3f1c16f1ebccd898
JOEL0055 <JOEL0055@e.ntu.edu.sg>
2024-08-24 19:35:49 +0800

    app.py

--- app1.py
+++ app1.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-app1 = Flask(__name__, template_folder="/Users/joelaiweithai/Downloads/BC3411/html/html_script")
+app1 = Flask(__name__)
 
 @app1.route("/", methods=["GET", "POST"])
 def index():
     return render_template("finance.html")
 
 if __name__ == "__main__":
-    # Specify a different port
     app1.run(port=5001)
